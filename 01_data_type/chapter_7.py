7994799f2efaa797c59568f858b040217788c4c4
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-09-11 18:58:26 +0530

    tuples

---
+++ 01_data_type/chapter_7.py
@@ -0,0 +1,12 @@
+# Tuples are immutable
+
+masala_spices = ("cardamom" , "cloves" , "cinnamon")
+(sp1 , sp2 , sp3) = masala_spices
+
+print(f"Masla spice {sp1 , sp2 , sp3}")
+
+
+#membership
+print(f"Is ginger in masala spice {'ginger' in masala_spices}")
+print(f"Is cinnamon in masala spice {'cinnamon' in masala_spices}")
+print(f"Is Cinnamon in masala spice {'Cinnamon' in masala_spices}")
