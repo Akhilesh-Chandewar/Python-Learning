a5e1b53d93be260f424cb86458cf467032e26dfb
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-09-11 18:30:50 +0530

    strings

---
+++ 01_data_type/chapter_6.py
@@ -0,0 +1,12 @@
+# strings and immutable
+
+chai_type = "Ginger_tea"
+customer_name = "Akhil"
+
+print(f"Order for {customer_name} : {chai_type} please!")
+
+chai_description = "Aromatic and Bold"
+print(f"first word {chai_description[0:9:2]}")
+print(f"last word {chai_description[12:]}")
+
+print(f"Reverse {chai_description[::-1]}")
