887a7ddc6c82bf0259c25ae31d620b03b9638522
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-09-13 13:34:57 +0530

    logic splitting

---
+++ 04_functions/02_complex.py
@@ -0,0 +1,16 @@
+def fetch_sales() :
+    print("Fetching sales")
+
+def filter_valid_saled():
+    print("Filtering valid sales")
+
+def summarize_data():
+    print("Summarizing data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_saled()
+    summarize_data()
+    print("Generating report")
+
+generate_report()
