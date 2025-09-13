185eb445034026107b76f7d20c56364b6249bc99
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-09-13 13:28:33 +0530

    duplications

---
+++ 04_functions/01_duplication.py
@@ -0,0 +1,6 @@
+def print_order(name = "person" , chai_type = "masala chai"):
+    print(f"Order for {name} : {chai_type} please!")
+    pass
+
+print_order("Akhil" , "coffee")
+print_order("superman" , "ginger tea")
