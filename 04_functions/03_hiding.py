616929995cb4e5f882bc40de853a64b94fd87665
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-09-14 13:03:30 +0530

    seperation of concern

---
+++ 04_functions/03_hiding.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("Getting user input")
+
+def validate_input():
+    print("validating user input")
+
+def save_to_db():
+    print("Saving to database")
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User registered")
+
+register_user()
