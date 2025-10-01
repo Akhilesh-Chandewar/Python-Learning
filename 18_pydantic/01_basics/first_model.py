9676d27780225d4e009225843a423279f5e05c58
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-10-01 13:55:54 +0530

    base model

--- 18_pydantic/01_basics/first_model.py
+++ 18_pydantic/01_basics/first_model.py
@@ -1,1 +1,15 @@
-print("Start of pydantic basics")
+from pydantic import BaseModel, Field
+
+class User(BaseModel):
+    id: int
+    name: str = Field(..., min_length=2, max_length=50)
+    age: int = Field(..., ge=0, le=120)
+    email: str
+    is_active: bool = True
+
+# Example usage
+try:
+    user = User(id=1, name="Alice", age=30, email="BqM0G@example.com")
+    print(user)
+except ValueError as e:
+    print("Validation error:", e)
